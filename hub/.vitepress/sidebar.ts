4f71e8ed6b7427655423d41176ddd20e9041c60a
alongw <alongw@alongw.cn>
2024-07-26 13:11:32 +0800

    feat: upload project

--- hub/.vitepress/sidebar.ts
+++ hub/.vitepress/sidebar.ts
@@ -1,19 +1,48 @@
 import type { DefaultTheme } from 'vitepress'
 
-const sidebar: DefaultTheme.Sidebar = [
-    {
-        text: 'Main',
-        items: [
-            {
-                text: 'Directory',
-                link: '/directory'
-            },
-            {
-                text: 'About',
-                link: '/about'
-            }
-        ]
-    }
-]
+const sidebar: DefaultTheme.Sidebar = {
+    '/': [
+        {
+            text: 'Main',
+            items: [
+                {
+                    text: 'Directory',
+                    link: '/directory'
+                },
+                {
+                    text: 'About',
+                    link: '/about'
+                }
+            ]
+        }
+    ],
+    '/projects/': [
+        {
+            text: 'Projects',
+            items: [
+                {
+                    text: '连续剧',
+                    collapsed: false,
+                    items: []
+                },
+                {
+                    text: '一般乐子',
+                    collapsed: false,
+                    items: [
+                        {
+                            text: 'MC 服务器后端大佬',
+                            link: '/projects/MC 服务器后端大佬'
+                        }
+                    ]
+                },
+                {
+                    text: '合集',
+                    collapsed: false,
+                    items: []
+                }
+            ]
+        }
+    ]
+}
 
 export default sidebar
